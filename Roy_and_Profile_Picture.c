fceefcea1c62d0f684475966c729be508dd16fc0
Ramavenkatp <119720285+Ramavenkatp@users.noreply.github.com>
2022-12-30 19:13:43 +0530

    #include<stdio.h> int main() {     int l,n;     scanf("%d%d",&l,&n);     for(int i=0;i<n;i++)     {         int w,h;         scanf("%d%d",&w,&h);         if(w<l||h<l)         {             printf("UPLOAD ANOTHER ");         }     else if(w>=l&&h>=l)     {         if(w==h)         {             printf("ACCEPTED ");         }         else         {             printf("CROP IT ");         }     }     } }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int l,n;
+    scanf("%d%d",&l,&n);
+    for(int i=0;i<n;i++)
+    {
+        int w,h;
+        scanf("%d%d",&w,&h);
+        if(w<l||h<l)
+        {
+            printf("UPLOAD ANOTHER
+");
+        }
+    else if(w>=l&&h>=l)
+    {
+        if(w==h)
+        {
+            printf("ACCEPTED
+");
+        }
+        else
+        {
+            printf("CROP IT
+");
+        }
+    }
+    }
+}

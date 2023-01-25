2d0f994d3544e2c00bad0a4a86ded2d4e8d6b993
Ramavenkatp <119720285+Ramavenkatp@users.noreply.github.com>
2023-01-25 10:18:28 +0530

    #include<stdio.h> int main() {     int num,count,fact,last_digit,sum=0,temp;     scanf("%d",&num);     for(temp=num;num>0;num=num/10)     {         fact=1;         last_digit=num%10;         for(count=1;count<=last_digit;count++)         {             fact=fact*count;         }         sum=sum+fact;     }     if(sum==temp)     {         printf("The number %d is a strong number",temp);     }     else     {         printf("The number %d is not a strong number",temp);     }     return 0; }

---
+++ Strong_Number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int num,count,fact,last_digit,sum=0,temp;
+    scanf("%d",&num);
+    for(temp=num;num>0;num=num/10)
+    {
+        fact=1;
+        last_digit=num%10;
+        for(count=1;count<=last_digit;count++)
+        {
+            fact=fact*count;
+        }
+        sum=sum+fact;
+    }
+    if(sum==temp)
+    {
+        printf("The number %d is a strong number",temp);
+    }
+    else
+    {
+        printf("The number %d is not a strong number",temp);
+    }
+    return 0;
+}
